2453a5396f0e7ede065804b925039cb02458b331
spadecompany-sangsoo <sangsoo@spadecomapny.kr>
2023-02-07 14:57:58 +0900

    Recoil 추가, container 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import {QueryClientProvider} from "@tanstack/react-query";
-import {QueryClient} from "@tanstack/query-core";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/query-core';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RecoilRoot } from 'recoil';
 
 const client = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <QueryClientProvider client={client}>
-            <div className="wrapper">
-                <Component {...pageProps} />
-            </div>
-            <ReactQueryDevtools />
-        </QueryClientProvider>
-    );
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={client}>
+        <div className="wrapper">
+          <Component {...pageProps} />
+        </div>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
